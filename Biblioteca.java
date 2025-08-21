957170a6edd444c809330262cd97f2918e2d5f6a
Thiago <thiago.smarques.bti@gmail.com>
2025-08-21 00:34:49 -0300

    Adicionou uma string

--- Biblioteca.java
+++ Biblioteca.java
@@ -2,5 +2,6 @@ public class Biblioteca {
     public static void main(String[] args) {
         System.out.println("Bem-vindo a Biblioteca Virtual!");
         int a = 1;
+        String b = "teste";
     }
 }
